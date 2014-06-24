61e1663cc6f0fde112f66ca1a3bec763c31cca9f
phiresky <phiresky@users.noreply.github.com>
2014-06-24 11:39:25 +0200

    fix algo

--- uebs.js
+++ uebs.js
@@ -2,7 +2,7 @@ var semester={
 	name:"KIT Inf SS2014",
 	uebs:{
 		"RO":{begin:"2014-04-28 13:15",every:7,url:"http://ti.ira.uka.de/TI-2/Uebungen/blatt{2}.pdf"},
-		"Algo":{begin:"2014-04-25 12:45",every:[7,7,7,7,7,7,7,14],url:"http://algo2.iti.kit.edu/documents/algo1-2014/blatt_{2}.pdf"},
+		"Algo":{begin:"2014-04-25 12:45",every:[7,7,7,7,7,7,7,14,7],url:"http://algo2.iti.kit.edu/documents/algo1-2014/blatt_{2}.pdf"},
 		"SWT":{begin:"2014-04-29 12:00",every:14,url:"http://svn.ipd.kit.edu/lehre/vorlesung/SWT1/SS14/stud/Uebung/{1}/"},
 		"HM":{begin:"2014-04-23 13:30",every:7,url:"http://www.math.kit.edu/iana3/lehre/hm2inf2014s/media/blatt_{1}.pdf"},
 		"LA":{begin:"2014-05-08 12:00",every:[7,7,14,7,7,7],url:"https://ilias.studium.kit.edu/goto_produktiv_fold_309670.html?dammitilias={2}"}
